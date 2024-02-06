8d84b25899b602d94bd04d86a26bfb6d3f8f433b
bryantanjw <bryantanjw01@gmail.com>
2024-02-05 23:10:15 -0700

    Added twitter link

--- client/components/user-nav.tsx
+++ client/components/user-nav.tsx
@@ -198,7 +198,7 @@ export function UserNav({ user, userDetails }: Props) {
             asChild
             className="text-muted-foreground cursor-pointer rounded-md py-2"
           >
-            <Link href="https://instagram.com/entropyso" target="_blank">
+            <Link href="https://twitter.com/bryantanjw" target="_blank">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="icon icon-tabler icon-tabler-message-circle w-5 h-5 mr-4"
@@ -214,7 +214,7 @@ export function UserNav({ user, userDetails }: Props) {
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M3 20l1.3 -3.9c-2.324 -3.437 -1.426 -7.872 2.1 -10.374c3.526 -2.501 8.59 -2.296 11.845 .48c3.255 2.777 3.695 7.266 1.029 10.501c-2.666 3.235 -7.615 4.215 -11.574 2.293l-4.7 1" />
               </svg>
-              Instagram
+              Twitter
             </Link>
           </DropdownMenuItem>
         </DropdownMenuGroup>
